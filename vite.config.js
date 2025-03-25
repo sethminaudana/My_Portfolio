af581ce0161fe13249ed989019cb38f51e84a115
Sethmina Udana <127284582+sethminaudana@users.noreply.github.com>
2025-03-25 16:25:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb", "**/*.gltf"],
+  base: '/My_Portfolio/',  // Replace with your repo name
   build: {
-    outDir: "build",
+    outDir: 'dist',
   },
 });
